2511a927bb8b8972cd88bffd8b88bab67a5d24ce
alex-njugi <alexnjugi11@gmail.com>
2025-05-25 16:09:20 +0300

    done

--- lib/department.py
+++ lib/department.py
@@ -1,5 +1,4 @@
 from lib import CURSOR, CONN
-from lib.employee import Employee
 
 class Department:
     all = {}
@@ -12,16 +11,14 @@ class Department:
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
 
-    # ==== Property Setters ====
-
     @property
     def name(self):
         return self._name
 
     @name.setter
-    def name(self, value):
-        if isinstance(value, str) and len(value.strip()) > 0:
-            self._name = value.strip()
+    def name(self, name):
+        if isinstance(name, str) and len(name.strip()):
+            self._name = name.strip()
         else:
             raise ValueError("Name must be a non-empty string")
 
@@ -30,13 +27,11 @@ class Department:
         return self._location
 
     @location.setter
-    def location(self, value):
-        if isinstance(value, str) and len(value.strip()) > 0:
-            self._location = value.strip()
+    def location(self, location):
+        if isinstance(location, str) and len(location.strip()):
+            self._location = location.strip()
         else:
             raise ValueError("Location must be a non-empty string")
-
-    # ==== Table methods ====
 
     @classmethod
     def create_table(cls):
@@ -52,35 +47,16 @@ class Department:
 
     @classmethod
     def drop_table(cls):
-        CURSOR.execute("DROP TABLE IF EXISTS departments")
+        sql = "DROP TABLE IF EXISTS departments"
+        CURSOR.execute(sql)
         CONN.commit()
 
     def save(self):
-        sql = """
-            INSERT INTO departments (name, location)
-            VALUES (?, ?)
-        """
+        sql = "INSERT INTO departments (name, location) VALUES (?, ?)"
         CURSOR.execute(sql, (self.name, self.location))
         CONN.commit()
         self.id = CURSOR.lastrowid
-        Department.all[self.id] = self
-
-    def update(self):
-        sql = """
-            UPDATE departments
-            SET name = ?, location = ?
-            WHERE id = ?
-        """
-        CURSOR.execute(sql, (self.name, self.location, self.id))
-        CONN.commit()
-
-    def delete(self):
-        sql = "DELETE FROM departments WHERE id = ?"
-        CURSOR.execute(sql, (self.id,))
-        CONN.commit()
-        if self.id in Department.all:
-            del Department.all[self.id]
-        self.id = None
+        type(self).all[self.id] = self
 
     @classmethod
     def create(cls, name, location):
@@ -88,17 +64,35 @@ class Department:
         dept.save()
         return dept
 
+    def update(self):
+        sql = "UPDATE departments SET name = ?, location = ? WHERE id = ?"
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        sql = "DELETE FROM departments WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+        if self.id in type(self).all:
+            del type(self).all[self.id]
+        self.id = None
+
     @classmethod
     def instance_from_db(cls, row):
-        dept_id = row[0]
-        if dept_id in cls.all:
-            dept = cls.all[dept_id]
+        dept = cls.all.get(row[0])
+        if dept:
             dept.name = row[1]
             dept.location = row[2]
         else:
-            dept = cls(row[1], row[2], dept_id)
-            cls.all[dept_id] = dept
+            dept = cls(row[1], row[2], row[0])
+            cls.all[row[0]] = dept
         return dept
+
+    @classmethod
+    def get_all(cls):
+        CURSOR.execute("SELECT * FROM departments")
+        rows = CURSOR.fetchall()
+        return [cls.instance_from_db(row) for row in rows]
 
     @classmethod
     def find_by_id(cls, id):
@@ -112,13 +106,8 @@ class Department:
         row = CURSOR.fetchone()
         return cls.instance_from_db(row) if row else None
 
-    @classmethod
-    def get_all(cls):
-        CURSOR.execute("SELECT * FROM departments")
-        rows = CURSOR.fetchall()
-        return [cls.instance_from_db(row) for row in rows]
-
     def employees(self):
+        from lib.employee import Employee  # correct import
         CURSOR.execute("SELECT * FROM employees WHERE department_id = ?", (self.id,))
         rows = CURSOR.fetchall()
         return [Employee.instance_from_db(row) for row in rows]
