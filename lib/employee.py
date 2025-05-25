a4c8845c3b81d0e905c8c603ab4a25335a794d11
alex-njugi <alexnjugi11@gmail.com>
2025-05-25 17:23:53 +0300

    Complete ORM lab: Review model and Employee relationship

--- lib/employee.py
+++ lib/employee.py
@@ -31,13 +31,44 @@ class Employee:
         CONN.commit()
 
     @classmethod
-    def find_by_id(cls, id):
-        CURSOR.execute("SELECT * FROM employees WHERE id = ?", (id,))
-        row = CURSOR.fetchone()
-        if row:
-            id, name, job_title, department_id = row
-            return cls(name, job_title, department_id, id)
-        return None
+    def create(cls, name, job_title, department_id):
+        employee = cls(name, job_title, department_id)
+        employee.save()
+        return employee
+
+    def save(self):
+        if self.id:
+            self.update()
+        else:
+            CURSOR.execute(
+                "INSERT INTO employees (name, job_title, department_id) VALUES (?, ?, ?)",
+                (self.name, self.job_title, self.department_id)
+            )
+            CONN.commit()
+            self.id = CURSOR.lastrowid
+            Employee.all[self.id] = self
+
+    @classmethod
+    def instance_from_db(cls, row):
+        id, name, job_title, department_id = row
+        if id in cls.all:
+            return cls.all[id]
+        employee = cls(name, job_title, department_id, id)
+        cls.all[id] = employee
+        return employee
+
+    def delete(self):
+        CURSOR.execute("DELETE FROM employees WHERE id = ?", (self.id,))
+        CONN.commit()
+        if self.id in Employee.all:
+            del Employee.all[self.id]
+        self.id = None
+
+    @classmethod
+    def get_all(cls):
+        CURSOR.execute("SELECT * FROM employees")
+        rows = CURSOR.fetchall()
+        return [cls.instance_from_db(row) for row in rows]
 
     def reviews(self):
         from lib.review import Review
